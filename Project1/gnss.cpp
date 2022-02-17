95987c3376517a9096dc9091a43449a9502cd7f7
HAE-CHANG LEE <gockdd1677@gmail.com>
2022-02-17 23:56:03 +0900

    commit today

--- Project1/gnss.cpp
+++ Project1/gnss.cpp
@@ -26,7 +26,7 @@ struct GNSS_f::eph {
 	double sqrtA;
 	double e;
 	double i_0, i_dot;
-	double omega, Omega_0;
+	double Omega_dot, omega, Omega_0;
 	double dn;
 	double C_uc, C_us;
 	double C_rc, C_rs;
@@ -44,7 +44,7 @@ struct GNSS_f::eph {
 		double sqrtA = 0;
 		double e = 0;
 		double i_0 = 0, i_dot = 0;
-		double omega = 0, Omega_0 = 0;
+		double Omega_dot = 0, omega = 0, Omega_0 = 0;
 		double dn = 0;
 		double C_uc = 0, C_us = 0;
 		double C_rc = 0, C_rs = 0;
@@ -58,6 +58,27 @@ struct GNSS_f::eph {
 	eph(int PPRN, double *V) {
 		prn = PPRN;
 		t_oe = V[1];
+		a = V[2];
+		b = V[3];
+		c = V[4];
+		sqrtA = V[12];
+		e = V[10];
+		i_0 = V[17];
+		i_dot = V[21];
+		Omega_dot = V[20];
+		omega = V[19];
+		Omega_0 = V[15];
+		dn = V[7];
+		C_uc = V[9];
+		C_us = V[11];
+		C_rc = V[18];
+		C_rs = V[6];
+		C_ic = V[14];
+		C_is = V[16];
+		IODE= V[5];
+		week_num = V[23];
+		health = V[26];
+		IODC = V[28];
 	}
 
 };
@@ -138,64 +159,64 @@ int GNSS_f::ReadEPH(std::string fp) {
 		int prn_n = std::stoi(line.substr(0, 2));
 		V[0] = prn_n;
 		V[1] = std::stod(line.substr(12, 13)) * 3600 + std::stod(line.substr(15, 16)) * 60 + std::stod(line.substr(18, 21)); // toe
-		V[2] = str2double(line, 22, 40); // a
-		V[3] = str2double(line, 41, 59); // b
-		V[4] = str2double(line, 60, 78); // c
+		V[2] = str2double(line, 22, 40); // a [sec]
+		V[3] = str2double(line, 41, 59); // b [sec/sec]
+		V[4] = str2double(line, 60, 78); // c [sec/sec2]
 		//std::cout << V[3] << std::endl;
 
 		// line 2
 		std::getline(input_file, line);
 
-		V[5] = str2double(line, 3, 21);
-		V[6] = str2double(line, 22, 40); 
-		V[7] = str2double(line, 41, 59); 
-		V[8] = str2double(line, 60, 78); 
+		V[5] = str2double(line, 3, 21); // IODE
+		V[6] = str2double(line, 22, 40); // Crs [meters]
+		V[7] = str2double(line, 41, 59); // dn [rad/sec]
+		V[8] = str2double(line, 60, 78); // M0 [rad]
 		//std::cout << V[8] << std::endl;
 
 		// line 3
 		std::getline(input_file, line);
 
-		V[9] = str2double(line, 3, 21);
-		V[10] = str2double(line, 22, 40);
-		V[11] = str2double(line, 41, 59);
-		V[12] = str2double(line, 60, 78);
+		V[9] = str2double(line, 3, 21); // Cuc [rad]
+		V[10] = str2double(line, 22, 40); // eccentricity
+		V[11] = str2double(line, 41, 59); //Cus [rad]
+		V[12] = str2double(line, 60, 78); //sqrt(A) [sqrt(m)]
 
 		// line 4
 		std::getline(input_file, line);
 
-		V[13] = str2double(line, 3, 21);
-		V[14] = str2double(line, 22, 40);
-		V[15] = str2double(line, 41, 59);
-		V[16] = str2double(line, 60, 78);
+		V[13] = str2double(line, 3, 21); // Toe [sec of GPS week]
+		V[14] = str2double(line, 22, 40); // Cic [rad]
+		V[15] = str2double(line, 41, 59); // Omega 0 [rad]
+		V[16] = str2double(line, 60, 78); // Cis [rad]
 
 		// line 5
 		std::getline(input_file, line);
 
-		V[17] = str2double(line, 3, 21);
-		V[18] = str2double(line, 22, 40);
-		V[19] = str2double(line, 41, 59);
-		V[20] = str2double(line, 60, 78);
+		V[17] = str2double(line, 3, 21); //i0 [rad]
+		V[18] = str2double(line, 22, 40); // Crc [meters]
+		V[19] = str2double(line, 41, 59); // omega [rad]
+		V[20] = str2double(line, 60, 78); // Omega dot [rad/sec]
 
 		// line 6
-		std::getline(input_file, line);
+		std::getline(input_file, line); 
 
-		V[21] = str2double(line, 3, 21);
-		V[22] = str2double(line, 22, 40);
-		V[23] = str2double(line, 41, 59);
-		V[24] = str2double(line, 60, 78);
+		V[21] = str2double(line, 3, 21); // IDOT [rad/sec]
+		V[22] = str2double(line, 22, 40); // Codes on L2 channel
+		V[23] = str2double(line, 41, 59); // GPS Week #
+		V[24] = str2double(line, 60, 78); // L2P data flag
 
 		// line 7
 		std::getline(input_file, line);
 
-		V[25] = str2double(line, 3, 21);
-		V[26] = str2double(line, 22, 40);
-		V[27] = str2double(line, 41, 59);
-		V[28] = str2double(line, 60, 78);
+		V[25] = str2double(line, 3, 21); // SV accuracy
+		V[26] = str2double(line, 22, 40); // SV health
+		V[27] = str2double(line, 41, 59); // TGD [sec]
+		V[28] = str2double(line, 60, 78); // IODC
 
 		// line 8
 		std::getline(input_file, line);
 
-		V[29] = str2double(line, 3, 21);
+		V[29] = str2double(line, 3, 21); // transmission time of message
 		V[30] = str2double(line, 22, 40);
 		//V[31] = str2double(line, 41, 59);
 		//V[32] = str2double(line, 60, 78);
