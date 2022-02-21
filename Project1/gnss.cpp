af4f454d42a97d0bf80fb27012745fcc5b956368
HAE-CHANG LEE <gockdd1677@gmail.com>
2022-02-21 20:33:17 +0900

    w

--- Project1/gnss.cpp
+++ Project1/gnss.cpp
@@ -1,5 +1,6 @@
 #include "gnss.h"
 //#include <string>
+
 #define MAXBUFLEN   1024
 
 GNSS_f::GNSS_f() {};
@@ -18,68 +19,7 @@ void GNSS_f::setDOY(std::string DOY) {
 }
 
 struct GNSS_f::eph {
-	char GNSS_type; // G R E C J ...
-
-	int prn;
-	double t_oe;
-	double a, b, c, t_gd;
-	double sqrtA;
-	double e;
-	double i_0, i_dot;
-	double Omega_dot, omega, Omega_0;
-	double dn;
-	double C_uc, C_us;
-	double C_rc, C_rs;
-	double C_ic, C_is;
-	double IODE;
-	double week_num;
-	double health;
-	double IODC;
-
-	eph() {
-
-		int prn=0;
-		double t_oe = 0;
-		double a = 0, b = 0, c = 0, t_gd = 0;
-		double sqrtA = 0;
-		double e = 0;
-		double i_0 = 0, i_dot = 0;
-		double Omega_dot = 0, omega = 0, Omega_0 = 0;
-		double dn = 0;
-		double C_uc = 0, C_us = 0;
-		double C_rc = 0, C_rs = 0;
-		double C_ic = 0, C_is = 0;
-		double IODE = 0;
-		double week_num = 0;
-		double health = 0;
-		double IODC = 0;
-	}
-
-	eph(int PPRN, double *V) {
-		prn = PPRN;
-		t_oe = V[1];
-		a = V[2];
-		b = V[3];
-		c = V[4];
-		sqrtA = V[12];
-		e = V[10];
-		i_0 = V[17];
-		i_dot = V[21];
-		Omega_dot = V[20];
-		omega = V[19];
-		Omega_0 = V[15];
-		dn = V[7];
-		C_uc = V[9];
-		C_us = V[11];
-		C_rc = V[18];
-		C_rs = V[6];
-		C_ic = V[14];
-		C_is = V[16];
-		IODE= V[5];
-		week_num = V[23];
-		health = V[26];
-		IODC = V[28];
-	}
+	
 
 };
 
@@ -220,7 +160,11 @@ int GNSS_f::ReadEPH(std::string fp) {
 		V[30] = str2double(line, 22, 40);
 		//V[31] = str2double(line, 41, 59);
 		//V[32] = str2double(line, 60, 78);
-		eph(prn_n, V);
+
+		
+		std::vector<eph> &a = eph(prn_n,V);
+		a(prn_n, V);
+		
 		break;
 	}
 
--- Project1/gnss.h
+++ Project1/gnss.h
@@ -21,7 +21,70 @@ public:
 	double str2double(std::string s, int a, int b);
 
 	int ReadEPH(std::string fp);
-	struct eph;
+	struct eph {
+		char GNSS_type; // G R E C J ...
+
+		int prn;
+		double t_oe;
+		double a, b, c, t_gd;
+		double sqrtA;
+		double e;
+		double i_0, i_dot;
+		double Omega_dot, omega, Omega_0;
+		double dn;
+		double C_uc, C_us;
+		double C_rc, C_rs;
+		double C_ic, C_is;
+		double IODE;
+		double week_num;
+		double health;
+		double IODC;
+
+		eph() {
+
+			int prn = 0;
+			double t_oe = 0;
+			double a = 0, b = 0, c = 0, t_gd = 0;
+			double sqrtA = 0;
+			double e = 0;
+			double i_0 = 0, i_dot = 0;
+			double Omega_dot = 0, omega = 0, Omega_0 = 0;
+			double dn = 0;
+			double C_uc = 0, C_us = 0;
+			double C_rc = 0, C_rs = 0;
+			double C_ic = 0, C_is = 0;
+			double IODE = 0;
+			double week_num = 0;
+			double health = 0;
+			double IODC = 0;
+		}
+
+		eph(int PPRN, double* V) {
+			prn = PPRN;
+			t_oe = V[1];
+			a = V[2];
+			b = V[3];
+			c = V[4];
+			sqrtA = V[12];
+			e = V[10];
+			i_0 = V[17];
+			i_dot = V[21];
+			Omega_dot = V[20];
+			omega = V[19];
+			Omega_0 = V[15];
+			dn = V[7];
+			C_uc = V[9];
+			C_us = V[11];
+			C_rc = V[18];
+			C_rs = V[6];
+			C_ic = V[14];
+			C_is = V[16];
+			IODE = V[5];
+			week_num = V[23];
+			health = V[26];
+			IODC = V[28];
+		}
+	};
 
 };
 
